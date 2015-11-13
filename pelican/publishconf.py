cddf8736c84bb5a297c8c79a4963b8cff0c2b2d3
40323108 <40323108@gm.nfu.edu.tw>
2015-11-13 21:50:53 +0800

    1113

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = 'KMOL'
-SITENAME = '2015FALL KMOL 課程'
+AUTHOR = '40323108'
+SITENAME = '2015FALL CADP '
 SITEURL = ''
 
 PATH = 'content'
