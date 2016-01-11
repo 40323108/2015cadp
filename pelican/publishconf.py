816522f08b175cea0d9446bdeaf1d6b4e200ca68
40323108 <40323108@gm.nfu.edu.tw>
2016-01-11 12:59:11 +0800

    0111

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -27,7 +27,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),('my github', 'https://github.com/40323108/2015cadp'),('全班網頁', 'http://2015fallhw.github.io/2015fallcadpa/user/40323108/'),
-         ('You can modify those links in your config file', '#'),)
+         ('my group', 'http://2015fallhw.github.io/2015fallcadpa/category/g1.html'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
